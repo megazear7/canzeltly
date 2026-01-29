086e682c403cb7ea2988ebce41e6813b780b22f7
Alex Lockhart <alexlockhart7@protonmail.com>
2026-01-29 06:22:40 -0500

    Apply formatting fixes to heads-up display component

--- src/client/component.heads-up-display.ts
+++ src/client/component.heads-up-display.ts
@@ -1,17 +1,20 @@
 import { css, html, LitElement, TemplateResult } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { customElement, property, query } from "lit/decorators.js";
 import { Game } from "../game/game.js";
 import { globalStyles } from "./styles.global.js";
+import { CanzeltlyModal } from "./component.modal.js";
 import "./component.modal.js";
 
 @customElement("canzeltly-heads-up-display")
 export class CanzeltlyHeadsUpDisplay extends LitElement {
-    @property({ attribute: false })
-    game?: Game;
+  @property({ attribute: false })
+  game?: Game;
 
-    static override styles = [
-        globalStyles,
-        css`
+  @query("canzeltly-modal") private menuModal?: CanzeltlyModal;
+
+  static override styles = [
+    globalStyles,
+    css`
       .hud {
         position: fixed;
         bottom: 0;
@@ -24,44 +27,50 @@ export class CanzeltlyHeadsUpDisplay extends LitElement {
         background-color: var(--color-secondary-surface);
         border-radius: var(--radius-medium) var(--radius-medium) 0 0;
         box-shadow: var(--shadow-normal);
+        width: 600px;
       }
     `,
-    ];
+  ];
 
-    private handleModalOpening(): void {
-        this.game?.pause();
-    }
+  private handleModalOpening(): void {
+    this.game?.pause();
+  }
 
-    private handleModalClosing(): void {
-        this.game?.resume();
-    }
+  private handleModalClosing(): void {
+    this.game?.resume();
+  }
 
-    private saveGame(): void {
-        throw new Error("Unimplemented");
-    }
+  private saveGame(): void {
+    throw new Error("Unimplemented");
+  }
 
-    private saveAndExit(): void {
-        throw new Error("Unimplemented");
-    }
+  private saveAndExit(): void {
+    throw new Error("Unimplemented");
+  }
 
-    private exitWithoutSaving(): void {
-        throw new Error("Unimplemented");
-    }
+  private exitWithoutSaving(): void {
+    throw new Error("Unimplemented");
+  }
 
-    override render(): TemplateResult {
-        return html`
-        <div class="hud">
-            <button class="pause-button">Pause</button>
+  private openModal(): () => void {
+    return () => {
+      this.menuModal?.open();
+    };
+  }
+
+  override render(): TemplateResult {
+    return html`
+      <div class="hud">
+        <button @click=${this.openModal()}>Pause</button>
+      </div>
+      <canzeltly-modal @modal-opening=${this.handleModalOpening} @modal-closing=${this.handleModalClosing}>
+        <div slot="body">
+          <h2>Game Menu</h2>
+          <button @click=${this.saveGame}>Save Game</button>
+          <button @click=${this.saveAndExit}>Save and Exit</button>
+          <button @click=${this.exitWithoutSaving}>Exit without Saving</button>
         </div>
-        <canzeltly-modal @modal-opening=${this.handleModalOpening} @modal-closing=${this.handleModalClosing}>
-            <button slot="open-button" class="pause-button">Pause</button>
-            <div slot="body" class="menu">
-            <h2>Game Menu</h2>
-            <button @click=${this.saveGame}>Save Game</button>
-            <button @click=${this.saveAndExit}>Save and Exit</button>
-            <button @click=${this.exitWithoutSaving}>Exit without Saving</button>
-            </div>
-        </canzeltly-modal>
+      </canzeltly-modal>
     `;
-    }
+  }
 }
