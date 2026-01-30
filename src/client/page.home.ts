97702c3d775eaef369f35402f19807e0c5f140b8
Alex Lockhart <alexlockhart7@protonmail.com>
2026-01-30 06:48:46 -0500

    Randomize quickstart game ID

--- src/client/page.home.ts
+++ src/client/page.home.ts
@@ -15,11 +15,12 @@ export class CanzeltlyHomePage extends CanzeltlyAppProvider {
   ];
 
   override render(): TemplateResult {
+    const randomId = Math.floor(Math.random() * 90000) + 10000;
     return html`
       <main>
         <img src="/logo/logo-512x512.png" alt="Canzeltly Logo" width="200" />
         <h1>Canzeltly</h1>
-        <p><a href="/play/default" class="standalone">Quick Start</a></p>
+        <p><a href="/play/quickstart-${randomId}" class="standalone">Quick Start</a></p>
         <p><a href="/create-game" class="standalone">Create Game</a></p>
         <p><a href="/saved-games" class="standalone">Saved Games</a></p>
       </main>
