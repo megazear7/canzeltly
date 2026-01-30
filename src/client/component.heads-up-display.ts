edcc37efce0c9a8073aff8f9ce287cf1cf617ef0
Alex Lockhart <alexlockhart7@protonmail.com>
2026-01-29 20:18:54 -0500

    Fix HUD positioning in nested shadow DOM and update plank images

--- src/client/component.heads-up-display.ts
+++ src/client/component.heads-up-display.ts
@@ -17,19 +17,19 @@ export class CanzeltlyHeadsUpDisplay extends LitElement {
     css`
       .hud {
         position: fixed;
-        bottom: 0;
+        z-index: 100;
+        width: 600px;
+        height: 83px;
+        bottom: 10px;
         left: 50%;
         transform: translateX(-50%);
         display: flex;
         justify-content: center;
         align-items: center;
-        padding: var(--size-large) var(--size-medium) var(--size-medium) var(--size-medium);
-        background-image: url("/images/plank.png");
-        background-size: cover;
+        background-image: url("/images/plank-1800x250.png");
+        background-size: contain;
         background-repeat: no-repeat;
         border-radius: var(--radius-medium) var(--radius-medium) 0 0;
-        box-shadow: var(--shadow-normal);
-        width: 600px;
       }
 
       .menu-options {
@@ -43,6 +43,24 @@ export class CanzeltlyHeadsUpDisplay extends LitElement {
       }
     `,
   ];
+
+  override render(): TemplateResult {
+    return html`
+      <div class="hud">
+        <button @click=${this.openModal()}>Menu</button>
+      </div>
+      <canzeltly-modal @modal-opening=${this.handleModalOpening} @modal-closing=${this.handleModalClosing}>
+        <div slot="body">
+          <h2>Game Menu</h2>
+          <div class="menu-options">
+            <button @click=${this.saveGame}>Save Game</button>
+            <button @click=${this.saveAndExit}>Save and Exit</button>
+            <button @click=${this.exitWithoutSaving}>Exit without Saving</button>
+          </div>
+        </div>
+      </canzeltly-modal>
+    `;
+  }
 
   private handleModalOpening(): void {
     this.game?.pause();
@@ -68,23 +86,5 @@ export class CanzeltlyHeadsUpDisplay extends LitElement {
     return () => {
       this.menuModal?.open();
     };
-  }
-
-  override render(): TemplateResult {
-    return html`
-      <div class="hud">
-        <button @click=${this.openModal()}>Menu</button>
-      </div>
-      <canzeltly-modal @modal-opening=${this.handleModalOpening} @modal-closing=${this.handleModalClosing}>
-        <div slot="body">
-          <h2>Game Menu</h2>
-          <div class="menu-options">
-            <button @click=${this.saveGame}>Save Game</button>
-            <button @click=${this.saveAndExit}>Save and Exit</button>
-            <button @click=${this.exitWithoutSaving}>Exit without Saving</button>
-          </div>
-        </div>
-      </canzeltly-modal>
-    `;
   }
 }
